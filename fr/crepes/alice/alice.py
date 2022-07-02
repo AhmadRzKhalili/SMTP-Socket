c0db2162d9449e7bad3c8c8a58f5f89ef8304d1b
AhmadRzKhalili <ahmadrkl2000@gmail.com>
2022-07-02 11:16:07 +0430

    Keep connection open - 7/2/2022

--- fr/crepes/alice/alice.py
+++ fr/crepes/alice/alice.py
@@ -1,12 +1,27 @@
-import socket            
- 
+import socket       
+import colorama
+from colorama import Fore     
+
+#who should close
+
 def stablish_connection():
-    s = socket.socket()
     s.connect((IP, PORT))
  
 PORT = 25  
 IP = '127.0.0.1'
+SIZE = 1024
+s = socket.socket()
 
-if __name__ == '__main__':
-    stablish_connection()
+stablish_connection()
+
+while True:
+    print(Fore.RED + "C: " ,  end = "")
+    req = input()
+    s.send(data.encode())
+    
+    if  req == '.':
+        s.close()
+        print(Fore.WHITE + "", end = "")
+        break
+
  
--- server.py
+++ server.py
@@ -1,10 +1,16 @@
-import socket			
+import socket		
+import os.path	
+import colorama
+from colorama import Fore  
+#who should close
+#how is the connection? new connection each time or one connection?
 
 s = socket.socket()		
 print("[CREATED] Socket successfully created.")
 
 PORT = 25
 NUM_CLINETS = 5
+SIZE = 1024
 
 s.bind(('', PORT))
 print("[BINDED] socket binded to %s" % (PORT))
@@ -13,10 +19,14 @@ s.listen(NUM_CLINETS)
 print("[LISTENING] socket is listening")	
 
 
-while True:
+c, addr = s.accept()
+print('[NEW CONNECTION] Got connection from', addr)
 
-    c, addr = s.accept()
-    print("")
-    print('[NEW CONNECTION] Got connection from', addr)
+while True:
+    data = c.recv(SIZE).decode()
+    print(data)
+    if data == '.':
+        c.close()
+        break
 
     
